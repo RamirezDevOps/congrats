11607cac5b5829592385d1107e62749522c09663
Anderson Ramirez <randerson@afyadux.org>
2023-09-11 12:20:45 -0500

    Add files via upload

---
+++ firebaseInit.js
@@ -0,0 +1,21 @@
+// firebaseInit.js
+
+// Import the required functions and libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzCUZYrLPCQc5oHjwL01B5khI9AtT4vEE",
+    authDomain: "coding-acceptance.firebaseapp.com",
+    projectId: "coding-acceptance",
+    storageBucket: "coding-acceptance.appspot.com",
+    messagingSenderId: "89757691018",
+    appId: "1:89757691018:web:c60e3c9f89c928e99e9173"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);  // Initialize Firestore
+
+export { db };  // This makes the Firestore instance available to other modules
